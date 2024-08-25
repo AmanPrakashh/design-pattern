ef0a27e9dc5e5a2253aec1829816db090123a494
AmanPrakashh <amanjhasahab@gmail.com>
2024-08-25 06:26:26 +0530

    decorator DP description

--- src/main/java/org/example/decorator/DecoratorTest.java
+++ src/main/java/org/example/decorator/DecoratorTest.java
@@ -2,6 +2,14 @@ package org.example.decorator;
 
 public class DecoratorTest {
     public static void main(String[] args) {
+
+
+        BaseConcreteClass baseConcreteClass = new BaseConcreteClass();
+        baseConcreteClass.functionToBeDecorated();
+
+        BaseDecorator baseDecorator = new BaseDecorator(new BaseConcreteClass());
+        baseDecorator.functionToBeDecorated();
+        System.out.println("*********************************************************");
         BaseInterface decoratorD1 = new DecoratorD1(new BaseConcreteClass());
         decoratorD1.functionToBeDecorated();
 
